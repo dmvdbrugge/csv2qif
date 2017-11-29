0b3a097439c91af47ef14c1ee19e7ea0fa8d7c07
Dave van der Brugge <dave@mollie.com>
2017-11-29 16:40:02 +0100

    Some `File` sanitisation

--- app/File/File.php
+++ app/File/File.php
@@ -18,13 +18,52 @@ class File
         $this->file = $file;
     }
 
+    /**
+     * @param string $mode
+     *
+     * @throws \Exception
+     */
     protected function open(string $mode = self::MODE_READ): void
     {
+        if ($this->isOpen()) {
+            throw new \BadMethodCallException("File {$this->file} already open!");
+        }
+
+        if ($mode !== self::MODE_READ && $mode !== self::MODE_WRITE) {
+            throw new \InvalidArgumentException("Unsupported mode {$mode}!");
+        }
+
+        if (file_exists($this->file)) {
+            if ($mode === self::MODE_READ && !is_readable($this->file)) {
+                throw new \Exception("File {$this->file} is not readable!");
+            } elseif ($mode === self::MODE_WRITE && !is_writable($this->file)) {
+                throw new \Exception("File {$this->file} is not writable!");
+            }
+        } else {
+            if ($mode === self::MODE_READ) {
+                throw new \Exception("File {$this->file} does not exist!");
+            } elseif (!($dirname = dirname($this->file)) || !is_writable($dirname)) {
+                throw new \Exception("Directory {$dirname} is not writable!");
+            }
+        }
+
         $this->handle = fopen($this->file, $mode);
     }
 
     protected function close(): void
     {
-        fclose($this->handle);
+        if ($this->isOpen()) {
+            fclose($this->handle);
+        }
+    }
+
+    protected function isOpen(): bool
+    {
+        return is_resource($this->handle);
+    }
+
+    public function __destruct()
+    {
+        $this->close();
     }
 }
