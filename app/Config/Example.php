2dfe487ccdc62b5823b59d8dbb7bdc96ef211519
Dave van der Brugge <dmvdbrugge@gmail.com>
2018-01-27 11:54:35 +0100

    When you manage multiple rulesets...
    in different config files, instead of all in the same one, parable
    overrides instead of merges, unless you use dot-notation.

--- app/Config/Example.php
+++ app/Config/Example.php
@@ -9,109 +9,106 @@ class Example implements Config
     public function get(): array
     {
         return [
-            // Namespace program
-            'csv2qif' => [
-                // This is ruleset 'example', for a personal account
-                'example'       => [
-                    'fallback' => true, // default
-                    'matchers' => [
-                        'Joint Account In'                                 => [
-                            'transfer'    => 'Income:Joint Account',
-                            'description' => ['getNoteDescription'], // default
-                            'rules'       => [
-                                ['equals', 'transfer', '<IBAN of Joint Account>'],
-                                ['greaterThan', 'amount', 0],
-                            ],
+            // This is ruleset 'example', for a personal account
+            'csv2qif.example'       => [
+                'fallback' => true, // default
+                'matchers' => [
+                    'Joint Account In'                                 => [
+                        'transfer'    => 'Income:Joint Account',
+                        'description' => ['getNoteDescription'], // default
+                        'rules'       => [
+                            ['equals', 'transfer', '<IBAN of Joint Account>'],
+                            ['greaterThan', 'amount', 0],
                         ],
-                        'Joint Account Ex'                                 => [
-                            'transfer' => 'Expenses:Joint Account',
-                            'rules'    => [
-                                ['equals', 'transfer', '<IBAN of Joint Account>'],
-                                ['lessThan', 'amount', 0],
-                            ],
+                    ],
+                    'Joint Account Ex'                                 => [
+                        'transfer' => 'Expenses:Joint Account',
+                        'rules'    => [
+                            ['equals', 'transfer', '<IBAN of Joint Account>'],
+                            ['lessThan', 'amount', 0],
                         ],
-                        'Father\'s House Ministries'                       => [
-                            'transfer' => 'Gifts:Father\'s House Ministries',
-                            'rules'    => [
-                                ['contains', 'description', 'Fathers House.Ministries'],
-                            ],
+                    ],
+                    'Father\'s House Ministries'                       => [
+                        'transfer' => 'Gifts:Father\'s House Ministries',
+                        'rules'    => [
+                            ['contains', 'description', 'Fathers House.Ministries'],
                         ],
-                        'Go and Tell Gifts'                                => [
-                            'transfer' => 'Gifts:Go and Tell',
-                            'rules'    => [
-                                ['contains', 'description', 'Go and Tell'],
-                                ['equals', 'notes->authorizationId', '12345'],
-                            ],
+                    ],
+                    'Go and Tell Gifts'                                => [
+                        'transfer' => 'Gifts:Go and Tell',
+                        'rules'    => [
+                            ['contains', 'description', 'Go and Tell'],
+                            ['equals', 'notes->authorizationId', '12345'],
                         ],
-                        'ING Costs'                                        => [
-                            'transfer' => 'Expenses:ING',
-                            'rules'    => [
-                                ['contains', 'description', 'Kosten'],
+                    ],
+                    'ING Costs'                                        => [
+                        'transfer' => 'Expenses:ING',
+                        'rules'    => [
+                            ['contains', 'description', 'Kosten'],
+                            [
+                                'oneOf',
+                                ['contains', 'notes->description', 'ING Bank'],
                                 [
-                                    'oneOf',
-                                    ['contains', 'notes->description', 'ING Bank'],
-                                    [
-                                        'allOf',
-                                        ['isEmpty', 'notes->description'],
-                                        ['contains', 'notes->source', 'ING Bank'],
-                                    ],
+                                    'allOf',
+                                    ['isEmpty', 'notes->description'],
+                                    ['contains', 'notes->source', 'ING Bank'],
                                 ],
                             ],
                         ],
-                        'Specsavers Contacts'                              => [
-                            'transfer' => 'Expenses:Specsavers:Contacts',
-                            'rules'    => [
-                                ['contains', 'description', 'Specsavers'],
-                                ['not', 'isEmpty', 'notes->authorizationId'],
-                            ],
+                    ],
+                    'Specsavers Contacts'                              => [
+                        'transfer' => 'Expenses:Specsavers:Contacts',
+                        'rules'    => [
+                            ['contains', 'description', 'Specsavers'],
+                            ['not', 'isEmpty', 'notes->authorizationId'],
                         ],
-                        'Specsavers Glasses'                               => [
-                            'transfer' => 'Expenses:Specsavers:Glasses',
-                            'rules'    => [
-                                ['contains', 'description', 'Specsavers'],
-                                ['isEmpty', 'notes->authorizationId'],
-                            ],
+                    ],
+                    'Specsavers Glasses'                               => [
+                        'transfer' => 'Expenses:Specsavers:Glasses',
+                        'rules'    => [
+                            ['contains', 'description', 'Specsavers'],
+                            ['isEmpty', 'notes->authorizationId'],
                         ],
-                        'Oranje Spaarrekening <account_number> Deposit'    => [
-                            'transfer'    => 'Assets:Bank:Savings',
-                            'description' => 'Deposit',
-                            'rules'       => [
-                                ['equals', 'account', '<Your IBAN>'],
-                                ['equals', 'notes->source', 'Naar Oranje Spaarrekening <account_number>'],
-                            ],
+                    ],
+                    'Oranje Spaarrekening <account_number> Deposit'    => [
+                        'transfer'    => 'Assets:Bank:Savings',
+                        'description' => 'Deposit',
+                        'rules'       => [
+                            ['equals', 'account', '<Your IBAN>'],
+                            ['equals', 'notes->source', 'Naar Oranje Spaarrekening <account_number>'],
                         ],
-                        'Oranje Spaarrekening <account_number> Withdrawal' => [
-                            'transfer'    => 'Assets:Bank:Savings',
-                            'description' => 'Withdrawal',
-                            'rules'       => [
-                                ['equals', 'account', '<Your IBAN>'],
-                                ['equals', 'notes->source', 'Van Oranje Spaarrekening <account_number>'],
-                            ],
+                    ],
+                    'Oranje Spaarrekening <account_number> Withdrawal' => [
+                        'transfer'    => 'Assets:Bank:Savings',
+                        'description' => 'Withdrawal',
+                        'rules'       => [
+                            ['equals', 'account', '<Your IBAN>'],
+                            ['equals', 'notes->source', 'Van Oranje Spaarrekening <account_number>'],
                         ],
                     ],
                 ],
-                // This ruleset is an example for the joint account referenced above, if you manage that separately.
-                'example-joint' => [
-                    'matchers' => [
-                        'Personal Account In' => [
-                            'transfer' => 'Income:Personal Account',
-                            'rules'    => [
-                                ['equals', 'transfer', '<IBAN of Personal Account>'],
-                                ['greaterThan', 'amount', 0],
-                            ],
+            ],
+            // This ruleset is an example for the joint account referenced above, if you manage that separately.
+            'csv2qif.example-joint' => [
+                'matchers' => [
+                    'Personal Account In' => [
+                        'transfer' => 'Income:Personal Account',
+                        'rules'    => [
+                            ['equals', 'transfer', '<IBAN of Personal Account>'],
+                            ['greaterThan', 'amount', 0],
                         ],
-                        'Personal Account Ex' => [
-                            'transfer' => 'Expenses:Personal Account',
-                            'rules'    => [
-                                ['equals', 'transfer', '<IBAN of Personal Account>'],
-                                ['lessThan', 'amount', 0],
-                            ],
+                    ],
+                    'Personal Account Ex' => [
+                        'transfer' => 'Expenses:Personal Account',
+                        'rules'    => [
+                            ['equals', 'transfer', '<IBAN of Personal Account>'],
+                            ['lessThan', 'amount', 0],
                         ],
-                        'Rent'                => [
-                            'transfer' => 'Expenses:Rent',
-                            'rules'    => [
+                    ],
+                    'Rent'                => [
+                        'transfer' => 'Expenses:Rent',
+                        'rules'    => [
 
-                            ],
                         ],
                     ],
                 ],
